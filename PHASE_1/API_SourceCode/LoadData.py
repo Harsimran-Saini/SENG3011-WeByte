c75e7a400cb59862a007e790a196a63a27be873b
Harsimran-Saini <41537268+Harsimran-Saini@users.noreply.github.com>
2021-03-15 23:54:38 +1100

    Update LoadData.py

--- PHASE_1/API_SourceCode/LoadData.py
+++ PHASE_1/API_SourceCode/LoadData.py
@@ -97,6 +97,6 @@ def load_disease(diseases, num, dynamodb=None):
 
 if __name__ == '__main__':
     disease_name = sys.argv[1]
-    with open(name_of_file', 'r') as json_file:
+    with open(name_of_file, 'r') as json_file:
         disease_list = json.load(json_file, parse_float=Decimal)
         load_disease(disease_list[disease_name], disease_list[disease_name+"num_articles"])
