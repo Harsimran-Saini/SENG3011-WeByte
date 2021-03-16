5d2b0f4fcacb5bd1f10e5e9662cb443c0334a8cc
Harsimran Saini <harsimransaini@Harsimrans-Air.lan>
2021-03-16 15:20:43 +1100

    Trial changes

--- PHASE_1/API_SourceCode/LoadData.py
+++ PHASE_1/API_SourceCode/LoadData.py
@@ -3,78 +3,99 @@ import json
 import boto3
 import sys
 from boto3.dynamodb.conditions import Key
+import boto3.dynamodb.types
+
+my_name = "unknown"
 
 def load_disease(diseases, num, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb', endpoint_url="https://dynamodb.ap-southeast-2.amazonaws.com")
 
-    table = dynamodb.Table('Data2')
-    table2 = dynamodb.Table('Keyword-frequencies')
 
-    response = table2.query(KeyConditionExpression=Key('Keyword').eq("other"))
-    table.update_item(
+    client = boto3.client('dynamodb')
+
+    response = client.update_item(
+        TableName = 'Keyword-frequencies',
         Key={
-            'Keyword': str('other')
+            "Keyword": {'S': my_name}
         },
-        UpdateExpression="set Number_of_articles =:i",
+        UpdateExpression="SET Number_of_articles = :val",
         ExpressionAttributeValues={
-            ':i': int(num)
-        },
-        ReturnValues="UPDATED_NEW"
+            ":val": {'N': num}
+        }
     )
+
     for d in diseases:
+        table = dynamodb.Table('Data2')
         response = table.query(KeyConditionExpression=Key('archive_id').eq(int(d['archive_id'])))
-        
         # if the id does not exist in the table
         if not response['Items']:
-            print()
-            print("hi")
-            print()
-            id = d['archive_id']
-            date = d['date']
-            disease = "other"
-            reports = d['reports']
-            main_text = d['main_text']
-            headline = d['headline']
-            url = d['url']
-            keywords = ["other"]
-            print("Adding disease:", id, disease)
-            table.put_item(Item=d)
+            response = client.put_item(
+                TableName = 'Data2',
+                Item={
+                    "archive_id": {'N': d['archive_id']},
+                    'date': {'S': d['date']},
+                    'disease': {'S': my_name},
+                    'reports': {'L': d['reports']},
+                    'main_text': {'S': d['main_text']},
+                    'headline': {'S': d['headline']},
+                    "url": {'S': d['url']},
+                    'keywords': {'SS': [my_name]}
+                }
+            )
+
+            response = client.update_item(
+                TableName = 'Data2',
+                Key={
+                    "archive_id": {'N': d['archive_id']},
+                    'date': {'S': d['date']}
+                },
+                UpdateExpression="set ",
+                ExpressionAttributeValues={
+                    'disease': {'S': my_name},
+                    'reports': {'L': d['reports']},
+                    'main_text': {'S': d['main_text']},
+                    'headline': {'S': d['headline']},
+                    "url": {'S': d['url']},
+                    'keywords': {'SS': [my_name]}
+                }
+            )
+
         # if the article id already exists
         else:
             # if there is already an attibute keywords
             if "keywords" in response['Items'][0].keys():
-                #if other is not in that keywords
-                if not ("other" in response['Items'][0]["keywords"]):
-                    table.update_item(
+                print("hi2")
+                #if my_name is not in that keywords
+                if my_name not in response['Items'][0]["keywords"]:
+                    print("wtf")
+                    response = client.update_item(
+                        TableName = 'Data2',
                         Key={
-                            'article_id': d['archive_id'],
-                            'date': d['date']
+                            "archive_id": {'N': d['archive_id']},
+                            'date': {'S': d['date']}
                         },
                         UpdateExpression="add keywords = list_append(keywords, :i)",
                         ExpressionAttributeValues={
-                            ':i': ["other"]
-                        },
-                        ReturnValues="UPDATED_NEW"
+                            ':i': {'SS': [my_name]}
+                        }
                     )
             # if there is no attribute keyword
             else:
-                table.update_item(
+                response = client.update_item(
+                    TableName = 'Data2',
                     Key={
-                        'article_id': d['archive_id'],
-                        'date': d['date']
+                        "archive_id": {'N': d['archive_id']},
+                        'date': {'S': d['date']}
                     },
                     UpdateExpression="set keywords =:i",
                     ExpressionAttributeValues={
-                        ':i': ["other"]
-                    },
-                    ReturnValues="UPDATED_NEW"
+                        ':i': {'SS': [my_name]}
+                    }
                 )
-
-
 
 if __name__ == '__main__':
     disease_name = sys.argv[1]
-    with open('txtfiles/other.txt', 'r') as json_file:
+    with open('txtfiles/unknown.txt', 'r') as json_file:
         disease_list = json.load(json_file, parse_float=Decimal)
         load_disease(disease_list[disease_name], disease_list[disease_name+"num_articles"])
