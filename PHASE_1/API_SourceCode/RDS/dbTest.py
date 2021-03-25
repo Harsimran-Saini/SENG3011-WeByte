374ba3522efeed641225df3f80010047e8cd8349
Lachlan Harvey <lachlan.harvey@student.unsw.edu.au>
2021-03-25 23:53:54 +1100

    trial of getting articles for scrape updates

--- PHASE_1/API_SourceCode/RDS/dbTest.py
+++ PHASE_1/API_SourceCode/RDS/dbTest.py
@@ -6,7 +6,7 @@ ENDPOINT="database-1.c8iucjwjdzap.ap-southeast-2.rds.amazonaws.com"
 PORT="5432"
 USR="postgres"
 REGION="ap-southeast-2"
-DBNAME="webyte"
+DBNAME="webyte_v2"
 
 #gets the credentials from .aws/credentials
 session = boto3.Session()
@@ -15,9 +15,11 @@ client = session.client('rds')
 try:
     conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USR, password="postgres")
     cur = conn.cursor()
-    cur.execute("""SELECT now()""")
-    query_results = cur.fetchall()
-    print(query_results)
+    cur.execute("""SELECT a.id from articles a
+    """)
+    # Make it a set for faster 'in' operation
+    idSet = {row[0] for row in cur.fetchall()}
+    print(idSet)
 except Exception as e:
     print("Database connection failed due to {}".format(e))                
                 
