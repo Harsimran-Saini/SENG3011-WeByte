871efa4733a2ef22ed4d84ab2771ade9e6e540ea
Lachlan Harvey <lachlan.harvey@student.unsw.edu.au>
2021-04-05 17:19:17 +1000

    added map style

--- PHASE_2/Application_SourceCode/epidemic-sandbox/src/components/Map.tsx
+++ PHASE_2/Application_SourceCode/epidemic-sandbox/src/components/Map.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
-import GoogleMapReact from 'google-map-react';
+import GoogleMap from 'google-map-react';
+import Styles from './MapStyle';
 
 const AnyReactComponent: any = ({text}: any) => <div>{text}</div>;
 
 const SimpleMap = (props: any) => {
+    const mapOptions = {
+      styles: Styles.styles
+    }
     const [center, setCenter] = useState({lat: 0, lng: 0 });
     const [zoom, setZoom] = useState(1);
     return (
         <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
+        <GoogleMap
           bootstrapURLKeys={{ key: 'AIzaSyCRQt-NkjJ1aa2PqGTu6y7zetkEqtl7s8w' }}
           defaultCenter={center}
           defaultZoom={zoom}
+          options={mapOptions}
+          
         >
           <AnyReactComponent
             lat={0}
             lng={0}
             text="My Marker"
           />
-        </GoogleMapReact>
+        </GoogleMap>
       </div>
     );
 }
